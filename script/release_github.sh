451d701e48f12de12fe1ab0740adccf1027de7cc
Saki Yakumo <yakumo@ziomatrix.org>
2016-05-11 16:13:02 +0900

    remove spaces

--- script/release_github.sh
+++ script/release_github.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # param1: filename ( file must be at $CIRCLE_ARTIFACTS )
-REPO_URL = https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/
+REPO_URL=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/
 if test $CIRCLE_BRANCH = "develop" then
-   REPO_URL = $REPO_URL/preview
+   REPO_URL=$REPO_URL/preview
 else
-   REPO_URL = `echo $REPO_URL/`cat ~/$CIRCLE_PROJECT_REPONAME/package.json | jq '.version'``
-fi; 
+   REPO_URL=`echo $REPO_URL/`cat ~/$CIRCLE_PROJECT_REPONAME/package.json | jq '.version'``
+fi
 
 UPLOAD_URL=`curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/preview | jq '.upload_url'`
 UPLOAD_URL=`echo $UPLOAD_URL | sed -e 's/{?name,label}/?name=/g' | sed -e 's/"//g'`
