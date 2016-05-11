296342fda2ce1a8f25e8bad30f2b2cd13fc1ad97
Saki Yakumo <yakumo@ziomatrix.org>
2016-05-11 17:23:05 +0900

    Github upload

--- script/github_upload.sh
+++ script/github_upload.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # param1: filename ( file must be at $CIRCLE_ARTIFACTS )
-REPO_URL=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/
-if test $CIRCLE_BRANCH = "develop" then
+REPO_BASE=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME
+REPO_URL=$REPO_BASE/releases/tags
+if test $CIRCLE_BRANCH = "develop"; then
    REPO_URL=$REPO_URL/preview
 else
    REPO_URL=`echo $REPO_URL/`cat ~/$CIRCLE_PROJECT_REPONAME/package.json | jq '.version'``
 fi
 
-UPLOAD_URL=`curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/preview | jq '.upload_url'`
+echo $REPO_URL
+JSON=`curl -H "Authorization: token $GITHUB_ACCESS_TOKEN" $REPO_URL`
+
+UPLOAD_URL=`echo $JSON | jq '.upload_url'`
 UPLOAD_URL=`echo $UPLOAD_URL | sed -e 's/{?name,label}/?name=/g' | sed -e 's/"//g'`
 
 UPLOAD_FILE=$CIRCLE_ARTIFACTS/$1
