07a88e6ad2cd16cc54f0410d5e775f17a47f6891
Saki Yakumo <yakumo@ziomatrix.org>
2016-05-12 02:14:20 +0900

    実ファイル名とGitHubファイル名を変えられるようにした

--- script/github_upload.sh
+++ script/github_upload.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # param1: filename ( file must be at $CIRCLE_ARTIFACTS )
+# param2: filename on github.
 REPO_BASE=https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME
 REPO_URL=$REPO_BASE/releases/tags
 if test $CIRCLE_BRANCH = "develop"; then
@@ -16,4 +17,4 @@ UPLOAD_URL=`echo $JSON | jq '.upload_url'`
 UPLOAD_URL=`echo $UPLOAD_URL | sed -e 's/{?name,label}/?name=/g' | sed -e 's/"//g'`
 
 UPLOAD_FILE=$CIRCLE_ARTIFACTS/$1
-curl --data-binary @$UPLOAD_FILE -H "Content-Type: application/zip" -H "Authorization: token $GITHUB_ACCESS_TOKEN" $UPLOAD_URL$1
+curl --data-binary @$UPLOAD_FILE -H "Content-Type: application/zip" -H "Authorization: token $GITHUB_ACCESS_TOKEN" $UPLOAD_URL$2
