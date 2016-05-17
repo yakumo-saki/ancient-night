5efa3a01f1d03ce8a6822c90cff1e11b87b832f9
Saki Yakumo <yakumo@ziomatrix.org>
2016-05-17 12:34:08 +0900

    retry #25

--- src/shared/GlobalSecret.ts
+++ src/shared/GlobalSecret.ts
@@ -1,4 +1,3 @@
 // 表に出せない情報
-// このファイルは --skip-worktree してある。
-export var CONSUMER_KEY:string = 'KEY';
-export var CONSUMER_SECRET:string = 'SECRET';
+export var CONSUMER_KEY:string = '___YOUR_KEY___';
+export var CONSUMER_SECRET:string = '___YOUR_SECRET___';
